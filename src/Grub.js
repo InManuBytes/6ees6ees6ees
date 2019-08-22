edb078713f8a2e7b0e67b64cb81879d36158cc4b
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-21 23:14:43 -0400

    Complete Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,17 @@
+//Create a Grub class, in ES6 syntax, with:
+/*
+ [x] an age property that is set to 0
+ [x] a color property that is set to 'pink'
+ [x] a food property that is set to jelly
+ [x] an eat method that returns 'Mmmmmmmmm jelly'
+ */
 class Grub {
-};
+  constructor () {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat () {
+    return 'Mmmmmmmmm jelly';
+  }
+}
